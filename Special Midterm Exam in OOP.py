b8300384ba3a6a19a8e5630021a6c4621246042a
Jeroh Mojica <90833736+Jerohlee@users.noreply.github.com>
2022-04-28 14:48:18 +0800

    Add files via upload

---
+++ Special Midterm Exam in OOP.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+window = Tk()
+
+window.title("Special Midterm Exam in OOP")
+window.geometry("300x200+20+10")
+
+def changecolor():
+ button.configure(bg="yellow")
+
+button = Button(window, text = "Click to Change Color", command= changecolor)
+button.place(relx=.5, y=100, anchor="center")
+
+window.mainloop()
