541bd7279b98013352e7b1c02ed0f86e7591669f
Jeroh Mojica <90833736+Jerohlee@users.noreply.github.com>
2022-05-20 00:50:40 +0800

    Add files via upload

---
+++ connect.py
@@ -0,0 +1,9 @@
+import pyodbc
+
+try:
+    connect = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\lee\Documents\Database01.accdb;')
+    print("Database is Connected")
+
+
+except pyodbc.Error:
+    print("Error in Connection")
